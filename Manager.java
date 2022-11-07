61c7efc75ff56210223a4c77c64ca43bd14e9f87
Rafael <rafael2mcont@gmail.com>
2022-11-07 19:18:40 -0300

    One exception fixed, but problem with users in activities persist.

--- Manager.java
+++ Manager.java
@@ -363,25 +363,27 @@ public class Manager extends Projetos implements MainManager{
 
                 System.out.println("Digite um ID de projeto para alterar o status:");
                 // opt.nextLine();
-                int id9 = Integer.parseInt(opt.nextLine());
-                String coord = projects.get(id9).getCoord();
-                boolean permission = projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PROFESSORA || projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PROFESSOR || projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PESQUISADORA || projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PESQUISADOR;
-                if (projectExist(id9)==true && projects.get(id9).getStatus()<3 && getLogin().getName().equals(projects.get(id9).getCoord()) && (permission==true)){
-                    System.out.print("Coord. "+projects.get(id9).getCoord()+", confirma a alteração de status de \""+projects.get(id9).getStringState()+"\"");
-                    projects.get(id9).setStatus(projects.get(id9).getStatus()+1);
-                    System.out.println(" para \""+projects.get(id9).getStringState()+"\"?");
-                    System.out.println("Digite 1 para confirmar ou outro número para não confirmar:");
-                    int confirm = Integer.parseInt(opt.nextLine());
-                    if (confirm==1){System.out.println("Alteração concluída");}
-                    else if (projects.get(id9).getStatus()>0){
-                        projects.get(id9).setStatus(projects.get(id9).getStatus()-1);
-                        System.out.println("Alteração não concluída.");
-                    }
-                    else{
-                        System.out.println("Alteração não concluída.");
+                try{
+                    int id9 = Integer.parseInt(opt.nextLine());
+                    String coord = projects.get(id9).getCoord();
+                    boolean permission = projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PROFESSORA || projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PROFESSOR || projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PESQUISADORA || projects.get(id9).getUserPointer(coord).getStatus()==UserStatus.PESQUISADOR;
+                    if (projectExist(id9)==true && projects.get(id9).getStatus()<3 && getLogin().getName().equals(projects.get(id9).getCoord()) && (permission==true)){
+                        System.out.print("Coord. "+projects.get(id9).getCoord()+", confirma a alteração de status de \""+projects.get(id9).getStringState()+"\"");
+                        projects.get(id9).setStatus(projects.get(id9).getStatus()+1);
+                        System.out.println(" para \""+projects.get(id9).getStringState()+"\"?");
+                        System.out.println("Digite 1 para confirmar ou outro número para não confirmar:");
+                        int confirm = Integer.parseInt(opt.nextLine());
+                        if (confirm==1){System.out.println("Alteração concluída");}
+                        else if (projects.get(id9).getStatus()>0){
+                            projects.get(id9).setStatus(projects.get(id9).getStatus()-1);
+                            System.out.println("Alteração não concluída.");
+                        }
+                        else{
+                            System.out.println("Alteração não concluída.");
+                        }
                     }
                 }
-                else{System.out.println("Erro. Projeto não existe, já concluído ou você não possui permissão para isso.");}
+                catch(NullPointerException e){System.out.println("Projeto não existe, já concluído ou você não possui permissão para isso. Erro: "+e);}
             }
             else if (option==10){
                 System.out.println("Escolhida a opção 10.");
